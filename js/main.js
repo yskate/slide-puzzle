c2f5327b916554ef5778bc9090f0703612b38345
Yskate <htbakt_miller@yahoo.co.jp>
2019-08-22 16:24:53 +0900

    ver2

--- js/main.js
+++ js/main.js
@@ -2,43 +2,144 @@
 
 {
 
-	const num = 3;
+	const contents = document.getElementsByClassName('content');
+	const selects = document.getElementsByClassName('level') ;
 
-	const frame = document.getElementById('frame');
+	
+	for(let i=0;i<selects.length;i++){
 
-	for(let i = 1; i <= num; i++){
-	for(let j = 1; j <= num; j++){
+		selects[i].addEventListener('click', function() {
 
-			const panel = document.createElement('div');
-			let n = (i-1) * 3 + j; 
-			
-			panel.classList.add('panel');
-			panel.id = 'panel' + n;
-			panel.dataset.map = String(i) + String(j);
-			mapping(panel);
+			contents[0].classList.remove('fade1');
+			contents[0].classList.add('fade2');
+			function fadein2(){
+				contents[1].classList.add('fade1');
+				contents[1].style.visibility='visible';
+				contents[0].style.display='none';
+			}
+			setTimeout(fadein2, 900);
+			let n =  Number(selects[i].dataset.level);
+
+			if(n){
+
+				let num=n;
+				let max = num * num;
+				
+				const frame = document.getElementById('frame');
+
+
+				if(num!=3){
+					let size = String(50*num) + 'px';
+					frame.style.width = size;
+					frame.style.height = size;
+				}
+
+				contents[1].style.height = String(250 + (num * 20)) + 'px';
 
 
 
-			if(n!=9){panel.textContent = n;}
-			
+				for(let i = 1; i <= num; i++){
+				for(let j = 1; j <= num; j++){
 
-			frame.appendChild(panel);
+						const panel = document.createElement('div');
+						let n = (i-1) * num + j; 
+						
+						panel.classList.add('panel');
+						panel.id = 'panel' + n;
+						panel.dataset.map = String(i) + String(j);
+						mapping(panel);
 
+
+
+						if(n!=max){panel.textContent = n;}
+						else{panel.style.backgroundColor = 'orange';}
+						
+
+						frame.appendChild(panel);
+						
+				}
+				}
+				
+
+				function direction (move_panel) {
+				
+					let d = Number(move_panel.dataset.map)-Number(document.getElementById('panel' + String(max)).dataset.map);
+				
+					return d;
+				}
+
+
+				function reset(){
+	
+					for(let i=1; i<=max; i++){
+				
+						let element = document.getElementById( 'panel' + i ) ;//パネル走査
+						element.classList.remove('point');//クラスリセット
+						if(i<=(max-1)){//空白パネル以外処理
+				
+							let d = direction(element);
+							let judge = Math.abs(d);
+				
+							if(judge==10 || judge==1){
+								element.classList.add('point');
+							}
+							
+							
+						}
+				
+				
+					}
+				
+				
+				}
+	
+	
+	
+				reset();
+
+				for(let i=1; i<=max; i++){
+				
+					let element = document.getElementById( 'panel' + i ) ;//パネル走査
+					if(i<=(max-1)){//空白パネル以外処理
+				
+						element.addEventListener('click', function() {
+							
+								
+								if(element.classList.contains('point')){
+
+								let p_max = document.getElementById('panel' + String(max));
+								let ex = element.dataset.map;
+								element.dataset.map = p_max.dataset.map;
+								p_max.dataset.map = ex;
+				
+								mapping(element);
+								mapping(p_max);
+
+								reset();
+								}
+							
+						
+						
+						});
+				
+						
+				
+					}
+				
+				
+				}
+
+
+			}
+
+		
+		});
+		
 	}
-	}
-
-
-
-
-
-	function direction (move_panel) {
 	
-		let d = Number(move_panel.dataset.map)-Number(document.getElementById('panel9').dataset.map);
-	
-		return d;
-	}
-	
-	
+
+
+
 	
 	function mapping(panel){
 	
@@ -51,71 +152,6 @@
 		panel.style.left = left;
 	
 	}
-	
-	
-	function reset(){
-	
-		for(let i=1; i<=9; i++){
-	
-			let element = document.getElementById( 'panel' + i ) ;//パネル走査
-			element.classList.remove('point');//クラスリセット
-			if(i<=8){//空白パネル以外処理
-	
-				let d = direction(element);
-				let judge = Math.abs(d);
-	
-				if(judge==10 || judge==1){
-					element.classList.add('point');
-				}
-				
-				
-			}
-	
-	
-		}
-	
-	
-	}
-	
-	
-	
-	reset();
-	
-	for(let i=1; i<=9; i++){
-	
-		let element = document.getElementById( 'panel' + i ) ;//パネル走査
-		if(i<=8){//空白パネル以外処理
-	
-			element.addEventListener('click', function() {
-				
-					
-					if(element.classList.contains('point')){
-
-					let p9 = document.getElementById('panel9');
-					let ex = element.dataset.map;
-					element.dataset.map = p9.dataset.map;
-					p9.dataset.map = ex;
-	
-					mapping(element);
-					mapping(p9);
-
-					reset();
-					}
-				
-			
-			
-			});
-	
-			
-	
-		}
-	
-	
-	}
-	
-
-			
-
 
 
 
